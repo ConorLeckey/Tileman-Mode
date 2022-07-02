415e57b1145a23df0ecf6783504c4d70bf31a57a
Elg <klink100@gmail.com>
2022-07-02 15:04:51 +0200

    Add more tests

--- src/test/java/com/tileman/MovementFlagTest.java
+++ src/test/java/com/tileman/MovementFlagTest.java
@@ -1,5 +1,6 @@
 package com.tileman;
 
+import static com.tileman.MovementFlag.NO_MOVEMENT_FLAGS;
 import static org.junit.Assert.*;
 
 import java.util.Set;
@@ -16,5 +17,46 @@ public class MovementFlagTest {
                     "Uncompressed does not contain the flag " + movementFlag.name(),
                     uncompressed.contains(movementFlag));
         }
+    }
+
+    @Test
+    public void compress_methods_does_the_same_thing_one_arg() {
+        MovementFlag[] movementFlags = {MovementFlag.BLOCK_MOVEMENT_NORTH_WEST};
+        int compressedArr = MovementFlag.compress(movementFlags);
+        int compressed = MovementFlag.compress(MovementFlag.BLOCK_MOVEMENT_NORTH_WEST);
+        assertEquals(compressedArr, compressed);
+    }
+
+    @Test
+    public void compress_methods_does_the_same_thing_two_args() {
+        MovementFlag[] movementFlags = {
+            MovementFlag.BLOCK_MOVEMENT_NORTH_WEST, MovementFlag.BLOCK_MOVEMENT_NORTH_EAST
+        };
+        int compressedArr = MovementFlag.compress(movementFlags);
+        int compressed =
+                MovementFlag.compress(
+                        MovementFlag.BLOCK_MOVEMENT_NORTH_WEST,
+                        MovementFlag.BLOCK_MOVEMENT_NORTH_EAST);
+        assertEquals(compressedArr, compressed);
+    }
+
+    @Test
+    public void containsAnyOf() {
+        int east = MovementFlag.compress(MovementFlag.BLOCK_MOVEMENT_EAST);
+        int west = MovementFlag.compress(MovementFlag.BLOCK_MOVEMENT_WEST);
+        int westEast =
+                MovementFlag.compress(
+                        MovementFlag.BLOCK_MOVEMENT_WEST, MovementFlag.BLOCK_MOVEMENT_EAST);
+
+        assertTrue(MovementFlag.containsAnyOf(east, westEast));
+        assertTrue(MovementFlag.containsAnyOf(west, westEast));
+        assertTrue(MovementFlag.containsAnyOf(westEast, east));
+        assertTrue(MovementFlag.containsAnyOf(westEast, west));
+
+        assertFalse(MovementFlag.containsAnyOf(west, east));
+        assertFalse(MovementFlag.containsAnyOf(NO_MOVEMENT_FLAGS, east));
+        assertFalse(MovementFlag.containsAnyOf(NO_MOVEMENT_FLAGS, west));
+        assertFalse(MovementFlag.containsAnyOf(west, NO_MOVEMENT_FLAGS));
+        assertFalse(MovementFlag.containsAnyOf(east, NO_MOVEMENT_FLAGS));
     }
 }
